93bbf23978c1e28f3ed336d6596dff31c910f93f
Jens Lafrentz <j.lafrentz@web.de>
2023-01-23 10:35:29 +0100

    fix: bug fix

--- script.js
+++ script.js
@@ -2,18 +2,8 @@ const fastCheck = document.querySelector("#fast");
 const cheapCheck = document.querySelector("#cheap");
 const goodCheck = document.querySelector("#good");
 last = null;
-/*
-function checker(item) {
-  if (fastCheck.checked && cheapCheck.checked && goodCheck.checked) {
-    last.checked = false;
-    last = item;
-  } else {
-    last = item; // 1. Durchlauf: last noch null !
-  }
-}
-*/
 
-// mein 1. Lösungsversuch (klappt!):
+// 1. Lösung über Array:
 let checkArr = [];
 function checker(item) {
   if (item.checked) {
@@ -38,36 +28,13 @@ cheapCheck.addEventListener("change", () => checker(cheapCheck));
 goodCheck.addEventListener("change", () => checker(goodCheck));
 
 /*
-  // andere Lösung (Klasse vor uns, ähnlich meiner):
-const fastInputElement = document.querySelector("#fast")
-const cheapInputElement = document.querySelector("#cheap")
-const goodInputElement = document.querySelector("#good")
-
-const saveActive = []; // max 3 elemente haben
-
-function check (item) {
-    if (item.checked) {
-        saveActive.push(item)
-    } else {
-        const index = saveActive.findIndex(currentArrItem => {
-            return currentArrItem === item
-        })
-
-        saveActive.splice(index, 1)
-    }
-  
-
-    if (saveActive.length > 2) {
-        saveActive[1].checked = false
-        saveActive.splice(1, 1)
-    }
-
-    console.log(saveActive)
+// 2. Lösung über Checkbox-Status
+function checker(item) {
+  if (fastCheck.checked && cheapCheck.checked && goodCheck.checked) {
+    last.checked = false;
+    last = item;
+  } else {
+    last = item; // 1. Durchlauf: last noch null !
+  }
 }
-
-// click event listener auf jedes Element
-fastInputElement.addEventListener("click", () => check(fastInputElement))
-cheapInputElement.addEventListener("click", () => check(cheapInputElement))
-goodInputElement.addEventListener("click", () => check(goodInputElement))
-
 */
